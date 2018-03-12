5d6459a6bb8dc6bba2b94833dec82c176ab8e2f7
vvopaa95 <chylikinmaksim@gmail.com>
2018-03-13 02:25:08 +0300

    react login to oauth2

---
+++ src/main/frontend/src/constants/settings.js
@@ -0,0 +1,8 @@
+let settings = {
+  production: {
+
+  },
+  develop: {
+
+  }
+};
--- src/main/frontend/src/containers/signing/SignIn.js
+++ src/main/frontend/src/containers/signing/SignIn.js
@@ -10,79 +10,155 @@ const steamStyle = {
   borderRadius:'20px'
 };
 
-//fetch('fsdf').then();
 
 class SignIn extends React.Component {
 
   constructor(props) {
     super(props);
-    this.login = this.login.bind(this);
+    this.onLogin = this.onLogin.bind(this);
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.state = {
       email: {
         value: '',
-        isValidInputClass: '',
-        isValidHintStyle: {
+        validInput: '',
+        validHint: {
+          display: 'none'
+        },
+        invalidHint: {
           display: 'none'
         }
       },
       password: {
         value: '',
-        isValidInputClass: '',
-        isValidHintStyle: {
+        validInput: '',
+        validHint: {
+          display: 'none'
+        },
+        invalidHint: {
           display: 'none'
         }
       },
     };
   }
 
-  login(e) {
-    if(this.state.email && this.state.password) {
-
+  onLogin(e) {
+    let error = false;
+    if(this.state.email.value) {
+      this.setState({
+        email: {
+          ...this.state.email,
+          validInput: 'is-valid',
+          validHint: {
+            display: 'block'
+          },
+          invalidHint: {
+            display: 'none'
+          }
+        }
+      });
+    }else{
+      this.setState({
+        email: {
+          ...this.state.email,
+          validInput: 'is-invalid',
+          validHint: {
+            display: 'none'
+          },
+          invalidHint: {
+            display: 'block'
+          }
+        }
+      });
+      error = true;
     }
+    if(this.state.password.value) {
+      this.setState({
+        password: {
+          ...this.state.password,
+          validInput: 'is-valid',
+          validHint: {
+            display: 'block'
+          },
+          invalidHint: {
+            display: 'none'
+          }
+        }
+      });
+    }else{
+      this.setState({
+        email: {
+          ...this.state.email,
+          validInput: 'is-invalid',
+          validHint: {
+            display: 'none'
+          },
+          invalidHint: {
+            display: 'block'
+          }
+        }
+      });
+      error = true;
+    }
+    if(error) return;
+    debugger;
+
+    fetch(window.location.host + '/oauth/token', {
+      method: 'post',
+      headers: {
+        'Authorization': 'Basic '+btoa('username:password'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: JSON.stringify({
+        'client_id': '(API KEY)',
+        'client_secret': '(API SECRET)',
+        'grant_type': 'client_credentials'
+      })
+    }).then()
   }
 
-  onChangeName = (e) => {
-    let currentName = e.target.value;
-    e.target.getAttribute('name');
+  onChangeName(e) {
     this.setState({
       email: {
         ...this.state.email,
-        value: currentName,
-        isValidInputClass: currentName ? 'is-valid' : 'is-invalid',
-        isValidHintStyle: !!currentName
+        value: e.target.value
       }
     });
-  };
+  }
 
   onChangePassword(e) {
-    let currentPassword = e.target.value;
+    this.setState({
+      password: {
+        ...this.state.password,
+        value: e.target.value
+      }
+    });
   }
 
   render() {
-    return (<section className="container">
+    return (
+      <section className="container">
         <Row>
           <Col className="d-flex justify-content-center align-items-center">
             <Form className="box-shadow-element-noborder m-4 p-4">
               <FormGroup>
                 <InputGroup>
                   <InputGroupAddon addonType="prepend"><InnerFormSvg svg={octicons.mail.toSVG()}/></InputGroupAddon>
-                  <Input type="email" name="email" id="userEmail" placeholder="my-mail@gmail.com" value={this.state.email.value} className={this.state.email.isValid} onChange={this.onChangeName} />
+                  <Input type="email" name="email" id="userEmail" placeholder="my-mail@gmail.com" value={this.state.email.value} className={this.state.email.validInput} onChange={this.onChangeName} />
                 </InputGroup>
-                <FormFeedback valid>This username is correct</FormFeedback>
-                <FormFeedback>The username mustn't be empty.</FormFeedback>
+                <FormFeedback valid style={this.state.email.validHint}>This username is correct</FormFeedback>
+                <FormFeedback style={this.state.email.invalidHint}>The username mustn't be empty.</FormFeedback>
               </FormGroup>
               <FormGroup>
                 <InputGroup>
                   <InputGroupAddon addonType="prepend"><InnerFormSvg svg={octicons.key.toSVG()}/></InputGroupAddon>
-                  <Input type="password" name="password" id="userPassword" placeholder="my-password123" value={this.state.password} className={this.state.isValidPass} onChange={this.onChangePassword} />
+                  <Input type="password" name="password" id="userPassword" placeholder="my-password123" value={this.state.password.value} className={this.state.password.validInput} onChange={this.onChangePassword} />
                 </InputGroup>
-                <FormFeedback valid>This password is correct</FormFeedback>
-                <FormFeedback>The password mustn't be empty.</FormFeedback>
+                <FormFeedback valid style={this.state.password.validHint}>This password is correct</FormFeedback>
+                <FormFeedback style={this.state.password.invalidHint}>The password mustn't be empty.</FormFeedback>
               </FormGroup>
               <FormGroup className="text-right">
-                <Button>Sign in</Button>
+                <Button onClick={this.onLogin}>Sign in</Button>
               </FormGroup>
               <hr/>
               <div className="text-center">
